be16b5b93654d4c60bb7bf15838db642b1887656
Alvin_Erngren_Te23E <alvin.erngren@elev.ga.ntig.se>
2024-09-09 14:50:14 +0200

    Tränat på heltal och konvertering med int.Parse()

---
+++ Kapitel-3/AlderGranser/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.Clear();
+Console.WriteLine("Hut gammal är du?");
+string setAge = Console.ReadLine();
+
+int age = int.Parse(setAge);
+
+if (age < 2)
+{
+    Console.WriteLine("Du får inte ens leka med lego");
+}
+else 
+{
+   if (age <= 99)
+   {
+     Console.WriteLine("Du får bygga lego.");
+   }
+   
+   if (age > 99)
+   {
+    Console.WriteLine("Du får inte bygga lego längre, tyvärr.");
+   }
+}
+
+if (age >= 12)
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine("Du får spela Fortnite!");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine("Du får inte spela Fortnite!");
+}
+
+if (age >= 18)
+{
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.WriteLine("Du får spela GTA V!");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.WriteLine("Du får inte spela GTA V.");
+}
+
+if (age >= 21)
+{
+    Console.WriteLine("Du får handla på systemet.");
+}
+else
+{
+    Console.WriteLine("Du får inte handla på systemet.");
+}
--- Kapitel-3/InteractiveStory/Program.cs
+++ Kapitel-3/InteractiveStory/Program.cs
@@ -13,8 +13,31 @@ Console.Write($"""
 Console.WriteLine("Where would you like to go? (left/back)");
 string answer = Console.ReadLine();
 
-// Kolla om svaret är left eller back
+// Kolla om svaret är left
 if (answer == "left")
 {
-Console.WriteLine("ok");
+Console.WriteLine("ok would you like to go down/left");
+answer = Console.ReadLine();
+if (answer == "left")
+{
+  Console.WriteLine("You found a treasure and became the king of the village")
+}
+if (answer == "down")
+{
+  Console.WriteLine("You found a knife and went to the dragons denn. Kill/run")
+  answer = Console.ReadLine();
+  if (answer == "Kill")
+  {
+    Console.WriteLine ("You tried to kill the dragon but a knife was apperently not enough and the dragen made BBQ out of you")
+  }
+  if (answer == "run")
+  {
+    Console.WriteLine ("You ran away but sliped on a banana peel (classic) and the knife sliped out of your hand and took your heart out")
+  }
+}
+}
+
+if (answer == "back")
+{
+Console.WriteLine("you died")
 }
