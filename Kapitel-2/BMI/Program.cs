6cdc97afa2b370811a2a40091a97921ba35f1a60
Alvin_Erngren_Te23E <alvin.erngren@elev.ga.ntig.se>
2024-09-11 14:59:47 +0200

    Biluthyrning

---
+++ Kapitel-2/BMI/Program.cs
@@ -0,0 +1,36 @@
+﻿// BMI räknare
+using System.Reflection;
+
+Console.Clear();
+Console.WriteLine("Räkna ut din BMI");
+
+// Ange vikt
+Console.WriteLine("Ange din vikt i kg:");
+double vikt = double.Parse(Console.ReadLine());
+
+// Ange längd
+Console.WriteLine("Ange din läng i meter:");
+double längd = double.Parse(Console.ReadLine());
+
+// Räkna ut BMI
+double BMI = vikt / (längd * längd);
+Console.WriteLine($"Din BMI är: {BMI}");
+
+// Beräkna om användaren är överviktig, underviktig eller i normalvikt
+if (BMI > 18.5)
+{
+   if (BMI > 25)
+   {
+    Console.WriteLine("Du är överviktig");
+   }
+
+   if (BMI < 25)
+   {
+    Console.WriteLine("Du är normalvikt");
+   }
+}
+else 
+{
+    Console.WriteLine("Du är underviktig");
+}
+
---
+++ Kapitel-2/Biluthyrning/Program.cs
@@ -0,0 +1,14 @@
+﻿// Biluthyrning
+Console.Clear();
+Console.WriteLine("Biluthyrning");
+
+// Ange antal mil och hur många dagar bilen ska lånas
+Console.WriteLine("Hur många mil ska du köra?");
+double sträcka = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Hur många dagar vill du låna bilen?");
+double dagar = double.Parse(Console.ReadLine());
+
+// Räkna ut och berätta kostnaden
+double kostnad = 500 + (100 * dagar + 5 * sträcka);
+Console.WriteLine($"Det kommer att kosta {kostnad}");
