b21284ba8a50d22ac2bc90251ecd4a118254fffb
Alvin_Erngren_Te23E <alvin.erngren@elev.ga.ntig.se>
2024-09-30 14:53:15 +0200

    Vi har tränat på List <> och slump och unicode och emoji

---
+++ Kapitel-5/KortSpel/Program.cs
@@ -0,0 +1,31 @@
+﻿// Ett annat exempel, lista på tre årtal
+// List<int> årtal = [1957, 1969, 1989];
+
+
+// Hur man skapar och använder listor
+Console.Clear();
+Console.WriteLine("Slumpa kort ur en kortlek!");
+
+// Skapa en lista kort 
+//List<string> kortlek = ["🂡", "🂢", "🂣", "🂤", "🂥", "🂦", "🂧", "🂨", "🂩", "🂪", "🂫", "🂭", "🂮"];
+List<string> kortlek = ["😱", "🂢", "🂣", "🂤", "🂥", "🂦", "🂧", "🂨", "🂩", "🂪", "🂫", "🂭", "🂮"];
+
+int antal = 5;
+while (antal > 0)
+{
+    // Slumpa index 0-12
+    int index = Random.Shared.Next(0, kortlek.Count);
+    //Console.WriteLine($"Det slumpade index = {index}");
+
+    // Plocka ut det sjunde kortet, index = 6
+    string kort = kortlek[index];
+
+    // Ta bort kortet ur kortleken
+    kortlek.RemoveAt(index);
+
+    // Skriv ut 7:e kortet
+    Console.WriteLine($"Det slumpade kortet är {kort}");
+
+    // Räkna ned
+    antal --;
+}
---
+++ Kapitel-5/SlumpaStader/Program.cs
@@ -0,0 +1,15 @@
+﻿// SLumpa 2 stad av 10
+Console.Clear();
+Console.WriteLine("Slumpa 2 av 10 städer");
+
+// Lista på städer
+List<string> Stader = ["Stockholm", "Paris", "Tokyo", "Vancouver", "Berlin", "Oslo", "New York City", "Seoul", "Hong kong", "Bangkok"];
+
+int antal = 2;
+while (antal > 0)
+{
+    int index = Random.Shared.Next(0, 9);
+    string stad = Stader[index];
+    Console.WriteLine($"Den slumpade staden blev {stad}");
+    antal --;
+}
