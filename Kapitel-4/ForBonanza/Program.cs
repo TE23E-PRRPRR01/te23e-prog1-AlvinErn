17f6437ca2aeb368e720dd9d4f1e2eab50ac0639
Alvin_Erngren_Te23E <alvin.erngren@elev.ga.ntig.se>
2024-11-20 15:00:30 +0100

    tränat på forloop

---
+++ Kapitel-4/ForBonanza/Program.cs
@@ -0,0 +1,19 @@
+﻿// ForBonanza
+Console.Clear();
+// Summa
+int summa = 0;
+// Ange start-tal och slut-tal
+System.Console.Write("Ange start-tal:");
+int startTal = int.Parse(Console.ReadLine());
+System.Console.Write("Ange slut-tal:");
+int slutTal = int.Parse(Console.ReadLine());
+while (true)
+{
+// For loop skriver heltal mellan två tal
+for (int i = startTal; i < slutTal + 1; i++)
+{
+    System.Console.WriteLine($"{i}");
+    summa = summa + i;
+}
+    System.Console.WriteLine($"Summan av sifrorna mellan {startTal} och {slutTal} = {summa}");
+}
