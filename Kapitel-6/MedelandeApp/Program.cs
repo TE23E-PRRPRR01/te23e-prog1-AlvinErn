2b6a8411dfc96cdb0d0c9f744f61784110a4c515
Alvin_Erngren_Te23E <alvin.erngren@elev.ga.ntig.se>
2025-01-22 15:05:44 +0100

    Miniräknare tränat på metoder och topdown strategi

---
+++ Kapitel-6/MedelandeApp/Program.cs
@@ -0,0 +1,88 @@
+﻿/* ************************************************
+ * **************** M A I N ***********************
+ ************************************************* */
+VisaPresentation();
+
+// Variabler
+string filnamn = "meddelande.txt";
+
+//Program-loop
+while (true)
+{
+    VisaMeny();
+    string val = Console.ReadLine();
+
+    if (val == "1")
+    {
+        SparaMedelande(filnamn);
+    }
+    else if (val == "2")
+    {
+        LäsaMedelande(filnamn);
+    }
+    else if (val == "3")
+    {
+        Console.WriteLine("Tack för idag");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Fel val! Var god försök igen!");
+    }
+}
+
+
+
+
+
+
+
+
+
+ /* ************************************************
+ * **************** Metoder ***********************
+ ************************************************* */
+ /// <summary>
+ /// Presentation av programet
+ /// </summary>
+static void VisaPresentation()
+
+{
+    Console.Clear();
+    Console.WriteLine("""
+    Program för att spara meddelande på disken
+    Av Bertil Karlsson 1987
+    """);
+    Console.WriteLine();
+}
+/// <summary>
+/// Visar programets meny med alla funktioner
+/// </summary>
+static void VisaMeny()
+{
+    Console.WriteLine("""
+    1. Spara meddelande
+    2. Läsa meddelande
+    3. Avsluta
+    """);
+    Console.Write("Ange ditt val: ");
+}
+/// <summary>
+/// Sparar meddelanden på textfilen
+/// </summary>
+static void SparaMedelande(string filen)
+{
+    Console.Write("Ange ett meddelande: ");
+    string meddelande = Console.ReadLine();
+    File.WriteAllText(filen, meddelande);
+    Console.WriteLine("Ditt meddelande har sparats!");
+}
+/// <summary>
+/// Läser upp meddelande från textfilen och skriver ut meddelandet
+/// </summary>
+static void LäsaMedelande(string filen)
+{
+    string meddelande = File.ReadAllText(filen);
+    Console.WriteLine("Ditt senaste meddelande:");
+    Console.WriteLine(meddelande);
+}
---
+++ Kapitel-6/Miniräknare/Program.cs
@@ -0,0 +1,104 @@
+﻿Intro();
+
+Meny();
+
+
+// Program-loop
+while (true)
+{
+if (condition)
+{
+UtförEnAddition();
+}
+else if (condition)
+{
+    UtförEnSubtraktion();
+}
+else if (condition)
+{
+    UtförEnMultiplikation
+}
+else if (condition)
+{
+    UtförEnDivision
+}
+else if (condition)
+{
+    Avsluta();
+}
+else
+{
+    Console.WriteLine("Felaktigt val! Var god försök igen");
+}
+}
+
+
+
+/* ************************************************
+ * **************** Metoder ***********************
+ ************************************************* */
+ static void Intro()
+ {
+    Console.Clear();
+    Console.WriteLine("Välkommen till miniräknaren (<_<)");
+    Console.WriteLine();
+ }
+ static void Meny()
+ {
+    Console.WriteLine("""
+    1. Utför en addition
+    2. Utför en subtraktion
+    3. Utför en multiplikation
+    4. Utför en division
+    5. Avsluta
+    """);
+    Console.Write("Ange ditt val: ");
+    string val = Console.ReadLine();
+    
+ }
+ static void UtförEnAddition()
+ {
+    Console.Clear();
+    Console.WriteLine("Utför en addition");
+    Console.Write("Ange heltal 1: ");
+    int heltal1 = int.Parse(Console.ReadLine());
+    Console.Write("Ange heltal 2: ");
+    int heltal2 = int.Parse(Console.ReadLine());
+    int AdderadeHeltal = heltal1 + heltal2;
+    Console.WriteLine($"Det blir {AdderadeHeltal}");
+    
+ }
+ static void UtförEnSubtraktion()
+ {
+    Console.Clear();
+    Console.WriteLine("Utför en subtraktion");
+    Console.Write("Ange heltal 1: ");
+    int heltal1 = int.Parse(Console.ReadLine());
+    Console.Write("Ange heltal 2: ");
+    int heltal2 = int.Parse(Console.ReadLine());
+    int SubtraheraHeltal = heltal1 - heltal2;
+    Console.WriteLine($"Det blir {SubtraheraHeltal}");
+ }
+ static void UtförEnMultiplikation()
+ {
+    Console.Clear();
+    Console.WriteLine("Utför en multiplikation");
+    Console.Write("Ange heltal 1: ");
+    int heltal1 = int.Parse(Console.ReadLine());
+    Console.Write("Ange heltal 2: ");
+    int heltal2 = int.Parse(Console.ReadLine());
+    int SubtraheraHeltal = heltal1 - heltal2;
+    Console.WriteLine($"Det blir {SubtraheraHeltal}");
+    
+ }
+ static void UtförEnDivision()
+ {
+    
+ }
+ static void Avsluta()
+ {
+    Console.WriteLine("Ha en trevlig dag!");
+    break;
+ }
+
+
